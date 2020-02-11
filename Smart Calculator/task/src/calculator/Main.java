df764e118aabf36b5f03c9d8a94dc246709f4083
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2020-02-11 14:41:40 +0100

    implemented tasks 1 to 3

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -6,6 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
+        loop:
+        while (true) {
+            String input = scanner.nextLine();
+            switch (input){
+                case "/help":
+                    System.out.println("The program calculates the sum of numbers");
+                    break;
+                case "/exit":
+                    System.out.println("Bye!");
+                    break loop;
+                case "":
+                    break;
+                default:
+                    System.out.println(calculateSum(input));
+                    break;
+            }
+        }
+    }
+
+    public static int calculateSum(String input) {
+        int sum = 0;
+        for(String number : input.split(" ")) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 }
